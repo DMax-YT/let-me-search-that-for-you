defa73c117675216b1df51aff8fb449f7f60e7c7
DMax-YT <maxim.borisov321@gmail.com>
2021-09-22 23:46:47 +0300

    Fix type

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <span>
-      Made with hate to people that can't search by
+      Made with hate to people who can't search by
       <a href="https://github.com/DMax-YT" target="_blank">DMax</a>
     </span>
   </footer>
